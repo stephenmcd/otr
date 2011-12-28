883b4eef948a89b4df3267995025cc573110980a
Stephen McDonald <steve@jupo.org>
2011-12-28 13:46:49 +1100

    Disable the content-type header to try and get Varnish working.

--- app.rb
+++ app.rb
@@ -7,9 +7,10 @@ get "/:username" do
 end
 
 get "/:gh_username/:bb_username" do
-  response["Cache-Control"] = "public, max-age=#{60*60*24}"
-  response["Content-Type"] = params[:callback] ? "text/javascript" \
-                                               : "application/json"
+  cache_for = 60*60*24
+  response["Cache-Control"] = "public, max-age=#{cache_for}"
+  # response["Content-Type"] = params[:callback] ? "text/javascript" \
+  #                                              : "application/json"
   output = OTR.get(params[:gh_username], params[:bb_username]).to_json
   output = "#{params[:callback]}(#{output});" if params[:callback]
   output
