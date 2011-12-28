cc286e7c8ff6aa30adae9c1298c68a232efb8314
Stephen McDonald <steve@jupo.org>
2011-12-28 16:42:27 +1100

    Added memcached.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source :rubygems
 
 gem "rest-client", "1.6.3"
 gem "sinatra", "1.2.6"
+gem "dalli", "1.1.4"
--- app.rb
+++ app.rb
@@ -1,17 +1,28 @@
 require "rubygems"
 require "sinatra"
+require "dalli"
 require "./lib/otr"
+
+CACHE_TIMEOUT = 60*60*24
+set :cache, Dalli::Client.new(ENV['MEMCACHE_SERVERS'] || "127.0.0.1",
+                             :username => ENV['MEMCACHE_USERNAME'],
+                             :password => ENV['MEMCACHE_PASSWORD'],
+                             :expires_in => CACHE_TIMEOUT)
 
 get "/:username" do
   call env.merge("PATH_INFO" => "/#{params[:username]}/#{params[:username]}")
 end
 
 get "/:gh_username/:bb_username" do
-  cache_for = 60*60*24
-  response["Cache-Control"] = "public, max-age=#{cache_for}"
-  # response["Content-Type"] = params[:callback] ? "text/javascript" \
-  #                                              : "application/json"
-  output = OTR.get(params[:gh_username], params[:bb_username]).to_json
+  response["Cache-Control"] = "public, max-age=#{CACHE_TIMEOUT}"
+  response["Content-Type"] = params[:callback] ? "text/javascript" \
+                                               : "application/json"
+  cache_key = "#{params[:gh_username]}/#{params[:bb_username]}/"
+  output = settings.cache.get cache_key
+  if output.nil?
+    output = OTR.get(params[:gh_username], params[:bb_username]).to_json
+    settings.cache.set cache_key, output
+  end
   output = "#{params[:callback]}(#{output});" if params[:callback]
   output
 end
